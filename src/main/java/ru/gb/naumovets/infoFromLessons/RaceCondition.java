c0d51aea40b42692ff6a55c441e7550a059ed0f6
Pavel Naumovets <pavel.naumovets@mail.ru>
2022-09-11 22:10:37 +0300

    last commit

---
+++ src/main/java/ru/gb/naumovets/infoFromLessons/RaceCondition.java
@@ -0,0 +1,82 @@
+package ru.gb.naumovets.infoFromLessons;
+
+public class RaceCondition {
+
+    //Монитор - контролирует доступ к объекту
+    public static class Counter {
+        private int value;
+
+        public int getValue() {
+            return value;
+        }
+
+        public synchronized void incrementValue() {
+            value++;
+        }
+
+        public synchronized void decrementValue() {
+            value--;
+        }
+    }
+
+    //Синхронизация метода через POJO (Plain Old Java Object - обычные джава объекты)
+    public static class SynchDoubleCounter {
+        private long first;
+        private long second;
+        private Object monFirst = new Object();
+        private Object monSecond = new Object();
+
+        public long getFirst() {
+            return first;
+        }
+
+        public long getSecond() {
+            return second;
+        }
+
+        public void incrementFirst() {
+            synchronized (monFirst) {
+                first++;
+            }
+        }
+
+        public void incrementSecond() {
+            synchronized (monSecond) {
+                second++;
+            }
+        }
+
+        public void decrementFirst() {
+            synchronized (monFirst) {
+                first--;
+            }
+        }
+
+        public void decrementSecond() {
+            synchronized (monSecond) {
+                second--;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                counter.incrementValue();
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                counter.decrementValue();
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(counter.getValue());
+    }
+}
+
---
+++ src/main/java/ru/gb/naumovets/infoFromLessons/ThreadCreation.java
@@ -0,0 +1,65 @@
+package ru.gb.naumovets.infoFromLessons;
+
+public class ThreadCreation {
+
+    public static void main(String[] args) {
+        //Первый способ запуска потока. Интерфейс ранабл
+        Thread thr1 = new Thread(new MyRunnableClass());
+        Thread thr2 = new Thread(new MyRunnableClass());
+        thr1.start();
+        System.out.println(thr1.getName());
+        thr2.start();
+        System.out.println(thr2.getName());
+
+
+        //Второй способ запуска потока. Класс Тред
+        Thread thr3 = new MyThreadClass();
+        Thread thr4 = new MyThreadClass();
+        thr3.start();
+        thr4.start();
+
+        //Третий способ запуска потока. Анонимный класс
+        Thread thr5 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println(i);
+                }
+            }
+        });
+
+        //.join. Ожидание завершения работы другим потоком
+        Thread thr6 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("*");
+            }
+        });
+        thr6.start();
+        try {
+            thr6.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("END");
+    }
+}
+
+class MyRunnableClass implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+class MyThreadClass extends Thread {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
